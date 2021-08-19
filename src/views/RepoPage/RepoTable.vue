ab75d1e972ed5f674b5416f28bff5674f3d9e631
Lindsay Mulhollen <lindsaymulhollen@gmail.com>
2021-08-19 12:36:14 -0500

    feat(tutorial): complete step 2

---
+++ src/views/RepoPage/RepoTable.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'RepoTable',
+  props: {
+    headers: Array,
+    rows: Array,
+    title: String,
+    helperText: String
+  },
+  computed: {
+    columns() {
+      return this.headers.map(header => header.header);
+    },
+    data() {
+      return this.rows.map(row => ({
+        data: [
+          row.name,
+          row.createdAt,
+          row.updatedAt,
+          row.issueCount,
+          row.stars,
+          row.links
+        ],
+        description: 'Row description'
+      }));
+    }
+  }
+};
+</script>
